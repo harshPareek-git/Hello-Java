2317f97b55927c3360ff5d1358cf387555f87fbe
harshPareek-git <harshpareek.git@gmail.com>
2024-02-06 14:26:47 +0530

    Maven project setup code

---
+++ src/main/java/org/hello_java/Arrays.java
@@ -0,0 +1,70 @@
+package org.hello_java;
+/*
+    This class has all the information about Arrays from Beginner to Advance
+ */
+public class Arrays {
+    public static void main(String[] args) {
+
+        /*
+        Beginner
+         */
+
+        /*
+        Ways to create Arrays in Java
+         */
+
+        // Array Declaration
+        int [] marks;
+
+        // Memory allocation
+        marks = new int[5];
+
+        /*
+         Declaration and Memory Allocation
+         int [] marks = new int [5]
+        */
+
+        marks[0] = 1;
+        marks[1] = 2;
+        marks[2] = 3;
+        marks[3] = 54;
+        marks[4] = 42;
+        System.out.println(marks[3]);
+
+        //Declaration and Initialization
+        int [] grade = {1,2,3,4,5};
+
+        /*
+        To Do
+        1) For each loop in array
+        2) Do multiple ops like reverse order printing using for each loop etc etc
+         */
+
+
+
+
+
+        /*
+            Intermediate
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          /*
+            Advance
+         */
+
+    }
+
+}
---
+++ src/main/java/org/hello_java/Main.java
@@ -0,0 +1,7 @@
+package org.hello_java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
